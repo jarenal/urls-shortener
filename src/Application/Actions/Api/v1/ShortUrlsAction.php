353c11a27df59c7d546c999697fd228cade78b7d
Jose Antonio <jguidos@hotmail.com>
2023-05-17 21:25:09 +0200

    Add /api/v1/short-urls endpoint skeleton

---
+++ src/Application/Actions/Api/v1/ShortUrlsAction.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Actions\Api\v1;
+
+use App\Application\Actions\Action;
+use Psr\Http\Message\ResponseInterface;
+
+class ShortUrlsAction extends Action
+{
+    protected function action(): ResponseInterface
+    {
+        return $this->respondWithData(['url' => $shortUrl]);
+    }
+}
